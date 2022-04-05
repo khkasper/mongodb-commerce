474ad442fcde397dea9226cde6af0be57417b254
Kristiano <kristianokasper@gmail.com>
2022-04-05 17:34:31 -0300

    Desafio 19 OK

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,4 +1,5 @@
-// Inclua bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo.
-// 1. Crie uma query que faça a inclusão de bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo.
+//Remova o item cebola de todos os sanduíches.
+// 1. Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+db.produtos.updateMany({}, { $pull: { ingredientes: "cebola" } });
 // 2. Crie uma query que retorne o nome e ingredientes de todos os documentos.
-
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
