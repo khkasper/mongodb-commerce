b0ce7dcccbc0559c20d1538d9cc6e157744d5a69
Kristiano <kristianokasper@gmail.com>
2022-04-05 17:08:13 -0300

    Desafio 14 OK

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,3 +1,4 @@
 // Crie uma query que retorne todos os lanches que possuem picles em seus ingredientes e mostre apenas os 3 primeiros itens contidos no array valoresNutricionais.
 // 1. Sua query deve retornar apenas os campos nome, ingredientes e valoresNutricionais.
 
+db.produtos.find({ ingredientes: "picles" }, { nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 }, _id: 0 });
