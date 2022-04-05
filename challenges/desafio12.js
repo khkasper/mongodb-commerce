6906a9ffddefda0b6180ba1459a002f273b445fd
Kristiano <kristianokasper@gmail.com>
2022-04-05 17:03:43 -0300

    Desafio 12 OK

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,3 +2,5 @@
 // 1. Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
 // 2. Crie uma query que retorne o nome e ingredientes de todos os documentos.
 
+db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } });
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
