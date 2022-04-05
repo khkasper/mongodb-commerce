e223bf688fd5d0eb06fb76430f5d0fead0bd83fc
Kristiano <kristianokasper@gmail.com>
2022-04-05 16:39:10 -0300

    Desafio 9 OK

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,2 +1,3 @@
 // Retorne o nome de todos os lanches que possuam calorias abaixo de 500.
 
+db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } }, { nome: 1, _id: 0 });
