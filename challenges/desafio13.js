7acb94995012f91ac4f3fc295827c4034d83bf25
Kristiano <kristianokasper@gmail.com>
2022-04-05 17:04:38 -0300

    Desafio 13 OK

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -2,3 +2,5 @@
 // 1. Crie uma query que adicione o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
 // 2. Crie uma query que retorne o nome e criadoPor de todos os produtos.
 
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+db.produtos.find({}, { nome: 1, criadoPor: 1, _id: 0 });
