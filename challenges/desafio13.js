e6aaf7cb0e0e90869dd13169f793588a0b57ebe6
Kristiano <kristianokasper@gmail.com>
2022-04-05 17:15:46 -0300

    Desafio 15 OK

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,6 +1,5 @@
 // Inclua o campo criadoPor em todos os documentos, colocando Ronald McDonald no valor desse campo.
 // 1. Crie uma query que adicione o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
-// 2. Crie uma query que retorne o nome e criadoPor de todos os produtos.
-
 db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+// 2. Crie uma query que retorne o nome e criadoPor de todos os produtos.
 db.produtos.find({}, { nome: 1, criadoPor: 1, _id: 0 });
